22a75042c1e2db0138dd14607db78bbde9ba17b9
Kevin <60135953+kev-ac@users.noreply.github.com>
2021-07-26 02:14:50 +0200

    Use HTML template

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const showdown = require('showdown');
+const util = require('util');
+
+const HTML_TEMPLATE = '<!DOCTYPE html><head><style type="text/css">body{font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";}</style></head><body>%s</body></html>';
 
 try {
   const markdownText = core.getInput('text');
   const converter = new showdown.Converter();
   const html = converter.makeHtml(markdownText);
 
-  core.setOutput("html", html);
+  const finalOutput = util.format(HTML_TEMPLATE, html);
+  
+  core.setOutput("html", finalOutput);
 
 } catch (error) {
   core.setFailed(error.message);
